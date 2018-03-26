2ba44b56ebd7bf4ce5b739b1439e0cc2e840570d
mromero3005 <cromeropedraza@ggc.edu>
2018-03-25 23:59:40 -0400

    formatting reviewed

--- src/MapMakerTile.java
+++ src/MapMakerTile.java
@@ -5,26 +5,32 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
-public class MapMakerTile extends JPanel{
-    int x, y;
-    
-    public MapMakerTile(int x, int y){
-        this.x = x;
-        this.y = y;
-        
-        addMouseListener(new MouseAdapter(){
-                    public void mousePressed(MouseEvent e) {
-                    	if(e.getButton() == MouseEvent.BUTTON1){
-	                        setBackground(Color.WHITE);
-	                        MazeMapMaker.map[x][y] = 1;
-                    	}
-                    	if(e.getButton() == MouseEvent.BUTTON3){
-	                        setBackground(Color.GRAY);
-	                        MazeMapMaker.map[x][y] = 0;
-                    	}
-                    }
-                    
- 
-                });
-    }
+public class MapMakerTile extends JPanel
+{
+	int x, y;
+
+	public MapMakerTile(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+
+		addMouseListener(new MouseAdapter()
+		{
+			public void mousePressed(MouseEvent e) 
+			{
+				if(e.getButton() == MouseEvent.BUTTON1)
+				{
+					setBackground(Color.WHITE);
+					MazeMapMaker.map[x][y] = 1;
+				}
+				if(e.getButton() == MouseEvent.BUTTON3)
+				{
+					setBackground(Color.GRAY);
+					MazeMapMaker.map[x][y] = 0;
+				}
+			}
+
+
+		});
+	}
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,16 +1,19 @@
 
 import javax.swing.JPanel;
 
-public class Tile extends JPanel{
-    int x, y;
-    boolean isWall = true;
-    
-    public Tile(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    public void setWall(boolean isWall){
-        this.isWall = isWall;
-    }
+public class Tile extends JPanel
+{
+	int x, y;
+	boolean isWall = true;
+
+	public Tile(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setWall(boolean isWall)
+	{
+		this.isWall = isWall;
+	}
 }
