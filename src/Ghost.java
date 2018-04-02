c156f0c08938e757bdf09dc99f5337074f69cb10
bojibenji <bbenji@ggc.edu>
2018-04-02 19:12:21 -0400

    Added to the original code and added some comments.

--- src/Ghost.java
+++ src/Ghost.java
@@ -2,7 +2,11 @@
  * Main boss: Pinky
  * Other Monsters: Blue Ghost0-4
  * Clyde, Inky, Blinky 
- * Each monster has their own stats 
+ * Each monster has their own stat's 
+ * ExamineGhost (user commands) shows description for the ghost. 
+ * Flee Ghost(user commands) 
+ * Deal/ Receive Damage 
+ * Each Ghost has a name, color, description, health, attack, defense, and accuracy. 
  */
 public class Ghost extends Person
 {
--- src/Puzzles.java
+++ src/Puzzles.java
@@ -1,4 +1,25 @@
 //Puzzle in this game should allows the user to move threw the maze easier (shortcuts) if the user answer the question correctly.
 public class Puzzles {
-
+/**
+ * Edited by Boji Benji
+ */
+	
+	//Examine puzzle, this would show the description of the puzzle 
+	public void examinePuzzle()
+	{
+		
+	}
+	
+	//This would allow the user to leave the puzzle
+	public void leavePuzzle()
+	{
+		
+	}
+	
+	
+	//This would ensure that something will happen upon solving the puzzle
+	public void solvePuzzle()
+	{
+		
+	}
 }
