742d203ab11ef1727fb5f5e1b760f27d05c4fbfa
bojibenji <bbenji@ggc.edu>
2018-04-02 18:24:34 -0400

    Added on the comment's made earlier

--- src/Items.java
+++ src/Items.java
@@ -1,8 +1,9 @@
 /*
- * We have 2 items 
- * Orbs (main item: gives user stats) 
- * Note (Puzzles)
- * Fruit (health item)
+ * We have 3 items 
+ * Orb's (main item: gives user stat's: Health, Defense, Attack) Each orb will increase attack, defense or health by 2, user choice. 
+ * Total 100 orb's in the game. 
+ * Note (Puzzles): Will be in random locations.
+ * Fruit (health item): Increase the user's health by 5 points 
  */
 public class Items 
 {
