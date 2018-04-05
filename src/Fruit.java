81cb87ede20397a17c55bc72ae0dadef906a07db
JCastro4 <JCastro3@GGC.edu>
2018-04-05 10:11:48 -0400

    Added variables, constructor, and method headers for Fruit class.

--- src/Fruit.java
+++ src/Fruit.java
@@ -3,11 +3,30 @@
  */
 
 /**
- * @author Cesar Mauricio Romero-Pedraza
- *Apr 4, 2018
- * Purpose
+ * @author Cesar Mauricio Romero-Pedraza Apr 4, 2018 Purpose
  */
 public class Fruit extends Items
 {
+	private int healthIncrement;
 	
+	public Fruit(int itemID, String itemName, String description, int healthIncrement)
+	{
+		super(itemID, itemName, description);
+		this.healthIncrement = healthIncrement;
+	}
+
+	public int getHealthIncrement()
+	{
+		return healthIncrement;
+	}
+
+	public void setHealthIncrement(int healthIncrement)
+	{
+		this.healthIncrement = healthIncrement;
+	}
+	
+	public void consumeFruit()
+	{
+
+	}
 }
