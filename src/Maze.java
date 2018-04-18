2b2c53e0b5f129664199fd07ccdb96053c4b4510
mromero3005 <cromeropedraza@ggc.edu>
2018-04-18 18:01:28 -0400

    maze orb count

--- src/Maze.java
+++ src/Maze.java
@@ -80,7 +80,7 @@ public class Maze extends JFrame
 				}
 
 //				if(player.x == columns-1 && player.y == endLevelLoc)
-				if(orbCount == 1)
+				if(orbCount == 5)
 				{
 					JOptionPane.showMessageDialog(null, "Congratulations, you've collected all 100 orbs", "Game Won!", JOptionPane.INFORMATION_MESSAGE);
 					dispose();
